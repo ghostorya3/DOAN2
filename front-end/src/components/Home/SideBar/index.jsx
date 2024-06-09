5128ac3b0cfe874d0eadfadd636507a0a5f05ae5
cuongnv <cuongnv@jitsinnovationlabs.com>
2024-06-09 19:18:23 +0700

    ga

--- front-end/src/components/Home/SideBar/index.jsx
+++ front-end/src/components/Home/SideBar/index.jsx
@@ -1,29 +1,30 @@
-import { GoPlus } from "react-icons/go";
-import { CiLogout } from "react-icons/ci";
-import Cookies from "js-cookie";
-import { useNavigate } from "react-router-dom";
-import { LuHome } from "react-icons/lu";
-import { FaCalendarDays } from "react-icons/fa6";
+import Image from "../../Image";
+import { FaPlus } from "react-icons/fa6";
+import { TiHome } from "react-icons/ti";
+import { FaRoad } from "react-icons/fa6";
+import { FaNewspaper } from "react-icons/fa";
 
-const SideBarLeft = () => {
-    const navigate = useNavigate();
-
+const Sidebar = () => {
     return (<>
-        <div className="h-full w-[64px] hover:w-[300px] p-2 transition-[width] duration-500 ease-in-out z-50 absolute flex flex-col gap-5 overflow-hidden border shadow-2xl bg-slate-50" >
-            <div className="flex gap-2 rounded-3xl border ml-[-25px] p-2 bg-blue-100">
-                <div className="w-max ml-5">
-                    <LuHome className="text-2xl text-gray-500"></LuHome>
-                </div>
-                <div className="overflow-hidden whitespace-nowrap ml-5 text-xl cursor-pointer text-gray-500" onClick={() => navigate(`/Home`)}>Màn hình chính</div>
+        <div className="w-28 py-5 px-3 flex justify-center items-center flex-col gap-3">
+            <div className="bg-blue-600 rounded-full border flex justify-center items-center cursor-pointer w-12 h-12">
+                <FaPlus className="text-white hover:rotate-45 hover:scale-150 transition-all duration-300"></FaPlus>
             </div>
-            <div className="flex gap-2 hover:rounded-3xl hover:border ml-[-25px] p-2">
-                <div className="w-max ml-5">
-                    <FaCalendarDays className="text-2xl text-gray-500"></FaCalendarDays>
-                </div>
-                <div className="overflow-hidden whitespace-nowrap ml-5 text-xl cursor-pointer text-gray-500">Việc cần làm</div>
+            <div className="flex flex-col items-center justify-center w-20 h-20 bg-stone-200 cursor-pointer rounded-2xl">
+                <TiHome className="text-xl"></TiHome>
+                <div className="text-xs font-medium mt-1">Trang chủ</div>
             </div>
-            
+            <div className="flex flex-col items-center justify-center w-20 h-20 hover:bg-stone-200 cursor-pointer rounded-2xl">
+                <FaRoad className="text-xl"></FaRoad>
+                <div className="text-xs font-medium mt-1">Lộ trình</div>
+            </div>
+            <div className="flex flex-col items-center justify-center w-20 h-20 hover:bg-stone-200 cursor-pointer rounded-2xl">
+                <FaNewspaper className="text-xl"></FaNewspaper>
+                <div className="text-xs font-medium mt-1">Bài viết</div>
+            </div>
         </div>
     </>)
 }
-export default SideBarLeft;
+
+export default Sidebar
+
--- front-end/src/routes/index.jsx
+++ front-end/src/routes/index.jsx
@@ -4,34 +4,18 @@ import {
 } from "react-router-dom";
 
 import Root from '../components/Root';
-import ErrorPage from "../pages/Error";
-import Login from '../pages/Login';
 import Home from "../pages/Home";
-import Work from "../pages/Work";
-import Class from "../pages/Class";
+
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root />,
-        errorElement: <ErrorPage />,
         children: [
             {
-                path: "/Work",
-                element: <Work />,
-            },
-            {
-                path: "/Home",
+                path: "/",
                 element: <Home />,
-            },
-            {
-                path: "/Class/:id",
-                element: <Class />,
-            },
-        ],
-    },
-    {
-        path: "/Login",
-        element: <Login />,
+            }
+        ]
     },
 
 ]);
