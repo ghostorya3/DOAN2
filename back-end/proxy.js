ac0bf94ad9f0db50729ab6175c3458cf922bd773
Cuongnv <cuongbatbai2001@gmail.com>
2024-07-13 03:55:42 +0700

    ga

--- back-end/proxy.js
+++ back-end/proxy.js
@@ -1,27 +1,43 @@
 var http = require('http');
 var httpProxy = require('http-proxy');
+var jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 var proxy = new httpProxy.createProxyServer({
-    target: {
-        host: 'localhost',
-        port: 8443
-    }
+  target: {
+    host: 'localhost',
+    port: 8443
+  }
 });
 
 var proxyServer = http.createServer(function (req, res) {
-    if (!req.url.includes('/?folder=/config/workspace')) {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('NotFound');
+
+  if (!req.url.includes('token')) {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    return res.end('NotFound');
+  }
+
+  let token = req.url.split('&')[0].replace('?token=', '')
+  if (!token) {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    return res.end('NotFound');
+  }
+  jwt.verify(token, process.env.PRIVATE_KEY, (err, decoded) => {
+    if (err || !req.url.includes(decoded?.data?.url)) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      return res.end('NotFound');
     }
-    proxy.web(req, res);
+  });
+
+  proxy.web(req, res);
 });
 
 proxyServer.on('upgrade', function (req, socket, head) {
-    proxy.ws(req, socket, head);
+  proxy.ws(req, socket, head);
 });
 
 proxyServer.listen(3001, function () {
-    console.log('Proxy server is running and listening on port 3001');
+  console.log('Proxy server is running and listening on port 3001');
 });
 
 
