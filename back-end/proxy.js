f829ecf4cf8cda6c841c96bcf4b766ca1f193cc4
Cuongnv <cuongbatbai2001@gmail.com>
2024-07-13 02:24:35 +0700

    ga

--- back-end/proxy.js
+++ back-end/proxy.js
@@ -2,22 +2,26 @@ var http = require('http');
 var httpProxy = require('http-proxy');
 
 var proxy = new httpProxy.createProxyServer({
-  target: {
-    host: 'localhost',
-    port: 8443
-  }
+    target: {
+        host: 'localhost',
+        port: 8443
+    }
 });
 
 var proxyServer = http.createServer(function (req, res) {
-  proxy.web(req, res);
+    if (!req.url.includes('/?folder=/config/workspace')) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('NotFound');
+    }
+    proxy.web(req, res);
 });
 
 proxyServer.on('upgrade', function (req, socket, head) {
-  proxy.ws(req, socket, head);
+    proxy.ws(req, socket, head);
 });
 
 proxyServer.listen(3001, function () {
-  console.log('Proxy server is running and listening on port 3001');
+    console.log('Proxy server is running and listening on port 3001');
 });
 
 
